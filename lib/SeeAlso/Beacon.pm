7343bf34368e1b055fea5a99aae52a5789ef877c
Jakob Voss <voss@gbv.de>
2010-10-26 15:17:51 +0000

    started implementing Beacon parser

--- lib/SeeAlso/Beacon.pm
+++ lib/SeeAlso/Beacon.pm
@@ -9,21 +9,27 @@ SeeAlso::Beacon - BEACON format parser and serializer
 
 =cut
 
+use Data::Validate::URI qw(is_uri);
+use Time::Piece;
+use Carp;
+
+our $VERSION = '0.10';
+
 =head1 SYNOPSIS
 
   use SeeAlso::Beacon;
 
-  $beacon = new SeeAlso::Beacon( $beaconfile );
+  my $beacon = new SeeAlso::Beacon( $beaconfile );
 
-  $beacon->meta( );
+  $beacon->meta(); # returns all meta fields as hash
+  $beacon->meta( 'DESCRIPTION' => 'my best links' ); # set meta fields
+  my $descr = $beacon->meta( 'DESCRIPTION' );        # get meta field
+  $beacon->meta( 'DESCRIPTION' => '' );              # unset meta field
 
-  $beacon->meta(); # return META fields as hash
-
-  $beacon->size(); # number of lines
-
-  $beacon->parse();
-
-  $beacon->query( $id );
+# Not implemented yet:
+#  $beacon->size(); # number of lines, parsed so far
+#  $beacon->parse( [$file], \&handler ); # parse all lines
+#  $beacon->query( $id );
 
 =head1 DESCRIPTION
 
@@ -38,34 +44,213 @@ Create a new Beacon object, optionally from a given file.
 =cut
 
 sub new {
+    my $class = shift;
+    my $self = bless { }, $class;
+    $self->_startparsing( @_ );
+    return $self;
+}
+
+=head2 meta ( [ $key [ => $value [ ... ] ] ] )
+
+Get and/or set one or more meta fields. Returns a hash (no arguments),
+or string or undef (one argument), or croaks on invalid arguments. A
+meta field can be unset by setting its value to the empty string.
+The FORMAT field cannot be unset. This method may also croak if supplied
+invalid field for known fields such as FORMAT, PREFIX, FEED, EXAMPLES,
+REVISIT, TIMESTAMP.
+
+=cut
+
+sub meta {
+    my $self = shift;
+    return %{$self->{meta}} unless @_;
+
+    if (@_ == 1) {
+        my $key = uc(shift @_);
+        $key =~ s/^\s+|\s+$//g;
+        return $self->{meta}->{$key};
+    }
+
+    croak('Wrong number of arguments in SeeAlso::Beacon->meta') if @_ % 2;
+
+    my %list = (@_);
+    foreach my $key (keys %list) {
+        croak('invalid meta name: "'.$key.'"') 
+            unless $key =~ /^\s*([a-zA-Z_-]+)\s*$/; 
+        my $value = $list{$key};
+        $key = uc($1);
+        $value =~ s/\s+|\s+$|\n//g;
+        if ($value eq '') { # empty field: unset
+            croak 'You cannot unset meta field #FORMAT' if $key eq 'FORMAT';
+            delete $self->{meta}->{$key};
+        } else { # check format of known meta fields
+            if ($key eq 'TARGET') {
+
+                # TODO
+
+            } elsif ($key eq 'FEED') {
+                croak 'FEED meta value must be a HTTP/HTTPS URL' 
+                    unless is_url($value);
+            } elsif ($key eq 'PREFIX') {
+                croak 'PREFIX meta value must be a URI' 
+                    unless is_uri($value);
+            } elsif ( $key =~ /^(REVISIT|TIMESTAMP)$/) {
+                if ($value =~ /^[0-9]+$/) { # seconds since epoch
+                    $value = gmtime($value)->datetime(); 
+                    # TODO: add warning about this conversion
+                } else {
+                    croak $key . ' meta value must be of form YYYY-MM-DDTHH:MM:SS'
+                        unless $value = Time::Piece->strptime( $value, '%Y-%m-%dT%T' );
+                    $value = $value->datetime();
+                }
+            } elsif ( $key eq 'FORMAT' ) {
+                croak 'Invalid FORMAT, must be BEACON or end with -BEACON'
+                    unless $value =~ /^([A-Z]+-)?BEACON$/;
+            } elsif ( $key eq 'EXAMPLES' ) {
+                my @examples = map { s/^\s+|\s+$//g; $_ } split '\|', $value;
+                $self->{examples} = [ grep { $_ ne '' } @examples ];
+                $value = join '|', @{$self->{examples}};
+                if ($value eq '') { # yet another edge case: "EXAMPLES: |" etc.
+                    delete $self->{meta}->{EXAMPLES};
+                    next;
+                }
+                # NOTE: examples are not checked for validity, we may need PREFIX first
+            }
+            $self->{meta}->{$key} = $value;
+        }
+    }
+}
+
+=head2 metastring 
+
+Return all meta fields, serialized and sorted as string.
+
+=cut
+
+sub metastring {
+    my $self = shift;
+    my %meta = $self->meta();
+    my @lines = '#FORMAT: ' . $meta{'FORMAT'};
+    delete $meta{'FORMAT'};
+    foreach my $key (keys %meta) {
+        push @lines, "#$key: " . $meta{$key}; 
+    }
+
+    return @lines ? join ("\n", @lines) . "\n" : "";
+}
+
+=head2 warning( $string )
+
+Adds a warning.
+
+=cut
+
+sub warning {
+    my ($self, $warning) = @_;
+    push @{$self->{warnings}}, $warning; # TODO: check this
+}
+
+=head2 parse ( [ $filename ] )
+
+Parse all remaining links. If provided a file name, this starts a new Beacon.
+That means the following is equivalent:
+
+  $b = new SeeAlso::Beacon( $filename );
+
+  $b = new SeeAlso::Beacon;
+  $b->parse( $filename );
+
+=cut
+
+sub parse {
+    my $self = shift;
+    $self->_startparsing( @_ );
+    
+    # TODO: parse all lines (IDs) via parselink
+}
+
+=head2 parselink ( $line )
+
+Parses a line, interpreted as link in BEACON format. Returns an array reference
+with four values on success, an empty array reference for empty linkes, or an 
+error string on failure. This method does not check whether the query identifier
+is a valid URI, because it may be expanded by a prefix.
+
+=cut
+
+sub parselink {
+    my ($self, $line) = @_;
+
+    my @parts = map { s/^\s+|\s$//g; $_ } split('\|',$line);
+    my $n = @parts;
+    return [] if ($n < 1 || $parts[0] eq '');
+    return "found too many parts (>4), divided by '|' characters" if $n > 4;
+    my $link = [shift @parts,"","",""];
+
+    $link->[3] = pop @parts
+        if ($n > 1 && is_uri($parts[$n-2]));
+
+    $link->[1] = shift @parts if @parts;
+    $link->[2] = shift @parts if @parts;
+
+    return "URI part has not valid URI form" if @parts; 
+
+    return $link;
+}
+
+=head1 INTERNAL METHODS
+
+=head2 _startparsing ( [ $filename ] )
+
+Open a BEACON file and start parsing by parsing all meta fields,
+and possibly the first link.
+
+=cut
+
+sub _startparsing {
+    my $self = shift;
+
+    $self->{meta} = { 'FORMAT' => 'BEACON' };
+    $self->{lineno} = 0;
+    $self->{examples} = [];
+    $self->{warnings} = [];
+
+    return unless @_;
+
+    # TODO: open file and parse all meta fields
+    my $filename = shift @_;
+
+    open $self->{fh}, $filename;
+    # TODO: check error on opening stream
+
+    # TODO: remove BOM (allowed in UTF-8)
+    # /^\xEF\xBB\xBF/
+    while (my $line = readline $self->{fh}) {
+        $line =~ s/^\s+|\s*\n?$//g;
+        next if $line eq '';
+        if ($line =~ /^#([^:=\s]+)(\s*[:=]?\s*|\s+)(.*)$/) {
+            $self->meta($1,$3);
+        } else {
+            my $link = $self->parselink( $line );
+            # TODO: handle $link
+            last;
+        }
+    }
+}
+
+=head2 is_url ( $string )
+
+Check whether a given String looks like a HTTP/HTTPS URL.
+
+=cut
+
+sub is_url {
+    return $_[0] =~ /^http(s)?:\/\/[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(\/[^#|]*)?(\?[^#|]*)?$/i;
 }
 
 1;
 
 __END__
-=head2 
-
-=cut
-
-sub {
-}
-
-=head2 
-
-=cut
-
-sub {
-}
-
-=head2 
-
-=cut
-
-sub {
-}
-1;
-
-#__END__
 
 =head1 AUTHOR
 
--- t/beacon.t
+++ t/beacon.t
@@ -1,6 +1,7 @@
 #!perl -Tw
 
 use strict;
+use warnings;
 
 use Test::More qw(no_plan);
 
@@ -35,5 +36,82 @@ is( $r->toBEACON(), join("\n",
  # "x|http://example.com"
 ) );
 
+use_ok('SeeAlso::Beacon');
 
-#is( $r->toBEACON(), "x||d|http://example.com" );
+my $b = new SeeAlso::Beacon();
+isa_ok($b,'SeeAlso::Beacon');
+
+# meta fields
+my %m = $b->meta();
+is_deeply( \%m, { 'FORMAT' => 'BEACON' }, 'meta()' );
+
+is_deeply( $b->meta('fOrMaT'), 'BEACON' );
+is_deeply( $b->meta('foo'), undef );
+is_deeply( $b->meta( {} ), undef );
+
+# not allowed or bad arguments
+eval { $b->meta( 'a','b','c' ); }; ok( $@ );
+eval { $b->meta( ' format' => '' ); }; ok( $@ );
+eval { $b->meta( ' ' => 'x' ); }; ok( $@ );
+eval { $b->meta( '~' => 'x' ); }; ok( $@ );
+eval { $b->meta( 'prefix' => 'htt' ); }; ok( $@ , 'detect invalid PREFIX');
+eval { $b->meta( 'Feed' => 'http://#' ); }; ok( $@ , 'detect invalid FEED');
+
+$b->meta( 'prefix' => 'http://foo.bar' );
+is_deeply( { $b->meta() }, { 'FORMAT' => 'BEACON', 'PREFIX' => 'http://foo.bar' } );
+$b->meta( 'prefix' => 'u:' ); # URI prefix
+$b->meta( 'prefix' => '' );
+
+eval { $b->meta( 'revisit' => 'Sun 3rd Nov, 1943' ); }; ok( $@ , 'detect invalid REVISIT');
+$b->meta( 'REvisit' => '2010-02-31T12:00:01' );
+is_deeply( { $b->meta() }, { 'FORMAT' => 'BEACON', 'REVISIT' => '2010-03-03T12:00:01' } );
+$b->meta( 'REVISIT' => '' );
+
+# not tested yet: FEED
+is( $b->meta( 'EXAMPLES' ), undef );
+$b->meta( 'EXAMPLES', 'foo | bar||doz ' );
+is( $b->meta('EXAMPLES'), 'foo|bar|doz', 'EXAMPLES' );
+$b->meta( 'EXAMPLES', '|' );
+is( $b->meta('EXAMPLES'), undef );
+$b->meta( 'EXAMPLES', '' );
+
+$b->meta('foo' => 'bar ', ' X ' => " Y\nZ");
+is_deeply( { $b->meta() }, { 'FORMAT' => 'BEACON', 'FOO' => 'bar', 'X' => 'YZ' } );
+$b->meta('foo',''); # unset
+is_deeply( { $b->meta() }, { 'FORMAT' => 'BEACON', 'X' => 'YZ' } );
+
+eval { $b->meta( 'format' => 'foo' ); }; ok( $@, 'detect invalid FORMAT' );
+$b->meta( 'format' => 'FOO-BEACON' );
+is( $b->meta('format'), 'FOO-BEACON' );
+
+# line parsing
+my %t = (
+  "qid" => ["qid","","",""],
+  "qid|\t" => ["qid","","",""],
+  "qid|" => ["qid","","",""],
+  "qid|lab" => ["qid","lab","",""],
+  "qid|lab|dsc" => ["qid","lab","dsc",""],
+  "qid| |dsc" => ["qid","","dsc",""],
+  "qid||dsc" => ["qid","","dsc",""],
+  "qid|u:ri" => ["qid","","","u:ri"],
+  "qid|lab|dsc|u:ri" => ["qid","lab","dsc","u:ri"],
+  "qid|lab|u:ri" => ["qid","lab","","u:ri"],
+  " \t" => [],
+  "" => [],
+  "qid|lab|dsc|u:ri|foo" => "found too many parts (>4), divided by '|' characters",
+  "|qid|u:ri" => [],
+  "qid|lab|dsc|abc" => "URI part has not valid URI form",
+);
+while (my ($line, $link) = each(%t)) {
+    $r = $b->parselink( $line );
+    is_deeply( $r, $link );
+}
+
+# file parsing
+$b = new SeeAlso::Beacon( "t/beacon1.txt" );
+is_deeply( { $b->meta() }, {
+  'FORMAT' => 'BEACON',
+  'TARGET' => 'http://example.com/{ID}',
+  'FOO' => 'bar',
+  'PREFIX' => 'x:'
+}, "parsing meta fields" );    
